9f0426b5bdde9a8f093975bab3662fdc3aa3aaf0
Shreya Ghate <ghate.s@northeastern.edu>
2021-12-02 13:29:44 -0500

    Update: EcoSystem and Config

--- CovidCareAED/src/Business/ConfigureASystem.java
+++ CovidCareAED/src/Business/ConfigureASystem.java
@@ -10,5 +10,20 @@ package Business;
  * @author shreya.ghate
  */
 public class ConfigureASystem {
-    
+    public static EcoSystem configure(){
+        
+        EcoSystem system = EcoSystem.getInstance();
+        
+        //Create a city for tracking Covid patients and vaccinations
+        //create an enterprise
+        //initialize some organizations
+        //have some employees or users
+        //create user account
+        
+        Employee employee = system.getEmployeeDirectory().createEmployee("sysadmin");
+        
+        UserAccount ua = system.getUserAccountDirectory().createUserAccount("sysadmin", "sysadmin", employee, new SystemAdminRole());
+        
+        return system;
+    }
 }
--- CovidCareAED/src/Business/EcoSystem.java
+++ CovidCareAED/src/Business/EcoSystem.java
@@ -5,10 +5,68 @@
  */
 package Business;
 
+import static java.lang.Integer.parseInt;
+import java.util.ArrayList;
+
 /**
  *
  * @author shreya.ghate
  */
 public class EcoSystem {
+    private static EcoSystem business;
+    private ArrayList<City> cityList;
+    public static EcoSystem getInstance(){
+        if(business==null){
+            business=new EcoSystem();
+        }
+        return business;
+    }
     
+    private EcoSystem(){
+        super(null, null, null, null, null, parseInt(null), null, null);
+        cityList=new ArrayList<City>();
+    }
+    
+    public City createAndAddCity(){
+        City city=new City();
+        cityList.add(city);
+        return city;
+    }
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roleList=new ArrayList<Role>();
+        roleList.add(new SystemAdminRole());
+        return roleList;
+    }
+
+    public ArrayList<City> getCityList() {
+        return cityList;
+    }
+
+    public void setCityList(ArrayList<City> cityList) {
+        this.cityList = cityList;
+    }
+    
+    public void removeCity(City n){
+        cityList.remove(n);
+    }
+    
+    public City getCity(String name){
+        for (City n: cityList){
+            if(n.getName().equalsIgnoreCase(name)){
+                return n;
+            }
+        }
+        return null;
+    }
+    
+    public boolean checkIfUserIsUnique(String userName){
+        if(!this.getUserAccountDirectory().checkIfUsernameIsUnique(userName)){
+            return false;
+        }
+        for(City city:cityList){
+            
+        }
+        return true;
+    }
 }
