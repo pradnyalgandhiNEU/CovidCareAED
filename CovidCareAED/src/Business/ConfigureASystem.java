b6d299e19096a56625127e7dcbf77227c65bafa1
Ayush_Gupta <gupta.ayus@northeastern.edu>
2021-12-04 18:48:29 -0500

    Employee changes

--- CovidCareAED/src/Business/ConfigureASystem.java
+++ CovidCareAED/src/Business/ConfigureASystem.java
@@ -1,14 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Business;
+
+import Business.Employee.Employee;
+import Business.Role.SystemAdminRole;
+import Business.UserAccount.UserAccount;
 
 /**
  *
- * @author shreya.ghate
+ * @author rrheg
  */
 public class ConfigureASystem {
+    
+    public static EcoSystem configure(){
+        
+        EcoSystem system = EcoSystem.getInstance();
+        
+        //Create a network
+        //create an enterprise
+        //initialize some organizations
+        //have some employees 
+        //create user account
+        
+        
+        Employee employee = system.getEmployeeDirectory().createEmployee("sysadmin");
+        
+        UserAccount ua = system.getUserAccountDirectory().createUserAccount("sysadmin", "sysadmin", employee, new SystemAdminRole());
+        
+        return system;
+    }
     
 }
