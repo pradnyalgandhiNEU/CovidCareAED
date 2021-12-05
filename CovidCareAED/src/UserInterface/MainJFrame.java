7c1be5ac3a2e1e3c6b9e5eaf07ee6f28b111c567
Pradnyal Gandhi <gandhi.prad@northeastern.edu>
2021-12-04 20:51:37 -0500

    Initialised personal branch

--- CovidCareAED/src/UserInterface/MainJFrame.java
+++ CovidCareAED/src/UserInterface/MainJFrame.java
@@ -65,7 +65,7 @@ public class MainJFrame extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addGap(192, 192, 192)
                         .addComponent(btnSubmit)))
-                .addContainerGap(105, Short.MAX_VALUE))
+                .addContainerGap(579, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -80,7 +80,7 @@ public class MainJFrame extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(lblPassword)
                     .addComponent(txtPassword, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 44, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 515, Short.MAX_VALUE)
                 .addComponent(btnSubmit)
                 .addGap(80, 80, 80))
         );
